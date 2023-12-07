8395962392dd47dff2d472290eb0aa5bddfff6e5
prabhatthakur672 <142176835+prabhatthakur672@users.noreply.github.com>
2023-12-07 10:36:48 +0530

    05_datatypes-summary.js

--- 01_basics/05_datatypes-summary.js
+++ 01_basics/05_datatypes-summary.js
@@ -35,3 +35,26 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+//+++++++++Stack(Primitive) & Heap(Non-Primitive)++++++++++++++
+
+//Stack
+let a=20;
+let a=b;
+b=10;
+console.log(a)  // out-20
+console.log(b) // out-10
+
+
+//Heap
+let userOne={
+    email:'pt672@gmail.com'
+};
+
+let userTwo=userOne;
+userTwo.email='iam123@outlook'
+
+console.log(userOne)  // out-iam123@outlook
+console.log(userTwo) // out-iam123@outlook
+
